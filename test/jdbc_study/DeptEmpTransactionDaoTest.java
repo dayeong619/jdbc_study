69696187123e1c3dc35506093a1041ba509fcade
김다영 <yohjk079@naver.com>
2019-06-22 15:21:50 +0900

    DeptEmpTransactionDaoTest so good!

--- test/jdbc_study/DeptEmpTransactionDaoTest.java
+++ test/jdbc_study/DeptEmpTransactionDaoTest.java
@@ -24,7 +24,8 @@ import jdbc_study.dto.Employee;
 public class DeptEmpTransactionDaoTest {
 	static final Logger log = LogManager.getLogger();
 	static DepartmentTransactionDao dao; //static은 static만 접근가능
-	
+	static DepartmentDao dao1;
+	static EmployeeDao dao2;
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
 		System.out.println();
@@ -43,10 +44,19 @@ public class DeptEmpTransactionDaoTest {
 	public void setUp() throws Exception {
 		System.out.println();
 	}
+	
+	@Test
+	public void test0transactionInsertEmpAndDept() throws SQLException {
+		dao1 = new DepartmentDaoImpl();
+		dao2 = new EmployeeDaoimpl();
+		dao2.deleteEmployee(new Employee(1005));
+		dao1.deleteDepartment(new Department(7));
+		
+	}
 
 	@Test
 	public void test1transactionInsertEmpAndDept() throws SQLException{
-		log.trace("test1transactionInsertEmpAndDept");
+		log.trace("test1");
 		Department dept = new Department(1, "영업", 1);
 		Employee emp = new Employee(1004, "서현진", "사원", new Employee(1033), 1500000, dept, null);
 		
@@ -59,7 +69,7 @@ public class DeptEmpTransactionDaoTest {
 	
 	@Test
 	public void test2transactionInsertEmpAndDept() throws SQLException {
-		log.trace("test2transactionInsertEmpAndDept");
+		log.trace("test2");
 		Department dept = new Department(8, "akzp", 1);
 		Employee emp = new Employee(1003, "조민희", "과장", new Employee(4377), 3000000, dept, null);
 		
@@ -72,20 +82,16 @@ public class DeptEmpTransactionDaoTest {
 
 	@Test
 	public void test3transactionInsertEmpAndDept() throws SQLException {
-		log.trace("test3transactionInsertEmpAndDept");
-		Department dept = new Department(7, "총무", 17);
+		log.trace("test3");
+		Department dept = new Department(7, "김다영", 17);
 		Employee emp = new Employee(1005, "이유영", "사원", new Employee(1003), 1500000, dept, null);
 		
 		int result = dao.trInsertEmpAndDept(emp, dept);
 		log.trace(String.format("result %d", result));
 		
-		Assert.assertNotEquals(2, result);
+		Assert.assertEquals(2, result);
 		
-		EmployeeDao empDao = new EmployeeDaoimpl();
-		empDao.deleteEmployee(emp);
 		
-		DepartmentDao deptDao = new DepartmentDaoImpl();
-		deptDao.deleteDepartment(dept);
 	}
 
 	
